4ca88137f348849828281ae6b50340dcc5c44c29
RomanVitolo <rvitolo@waypoint.la>
2023-11-07 16:06:55 -0300

    Fixes

---
+++ FPS-Assault - Roman Vitolo/Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Node : MonoBehaviour
+{
+    public List<Node> Neightbourds = new List<Node>();
+}
